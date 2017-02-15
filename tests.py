99d9535de8285a4092e3778abf4e9bf24d938830
BommireddyJahnavi <jahnavi160897@gmail.com>
2017-02-15 21:38:54 +0530

    Update tests.py

--- tests.py
+++ tests.py
@@ -1,14 +1,6 @@
-from django.test import TestCase
+def add(a, b):
+	return a + b;
 
-class SimpleTest(TestCase):
-    def test_basic_addition(self):
-        """
-        Tests that 1 + 1 always equals 2.
-        """
-        self.failUnlessEqual(1 + 1, 2)
-
-__test__ = {"doctest": """
-Another way to test that 1 + 1 is equal to 2.
->>> 1 + 1 == 2
-True
-"""}
+def add_numbers():
+    assert add(4, 5) == 9
+    assert add(4, 7) == 11
