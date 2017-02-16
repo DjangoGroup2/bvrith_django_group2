c6c2350f086bb931f91e4dcfec9b95c13436ca51
Bhuvija Priya <bhuvijayerra@gmail.com>
2017-02-16 14:13:23 +0530

    Update tests.py

--- tests.py
+++ tests.py
@@ -1,6 +1,6 @@
-def add(a, b):
-	return a + b;
+def multiply(a, b):
+	return a * b;
 
-def add_numbers():
-    assert add(4, 5) == 9
-    assert add(4, 7) == 11
+def test_numbers():
+    assert multiply(7, 5) == 35
+    assert multiply(4, 7) == 28
