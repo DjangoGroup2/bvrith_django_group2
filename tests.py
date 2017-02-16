8bc845ac12faa06678ef4ca58c7cc4aa0cc186c1
BommireddyJahnavi <jahnavi160897@gmail.com>
2017-02-16 14:16:22 +0530

    Update tests.py

--- tests.py
+++ tests.py
@@ -2,5 +2,5 @@ def multiply(a, b):
 	return a * b;
 
 def test_numbers():
-    assert multiply(7, 5) == 35
-    assert multiply(4, 7) == 28
+    assert multiply(2, 3) == 6
+    assert multiply(5, 4) == 20
