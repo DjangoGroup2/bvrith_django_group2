d5595c252f42ba423c00096dfd238e3c06ecd89a
SravyaKosuri <sravyakosuri1@gmail.com>
2017-03-22 22:03:04 +0530

    Add files via upload

--- models.py
+++ models.py
@@ -1,37 +1,18 @@
 from django.db import models
+
 class faculty(models.Model):
-        fid = models.CharField(max_length=9, primary_key=True)
-        fname = models.CharField(max_length=40)
-
-        def __unicode__(self):
-               return u'%s, %s' % (self.fid, self.fname)
+fid = models.CharField(max_length=9, primary_key=True)
 
 
-class course(models.Model):
-        cnum = models.CharField(max_length=9, primary_key=True)
-        cname = models.CharField(max_length=40)
-        credits = models.IntegerField()
+class student(models.model):
+student_Id = models.ForeignKey(personalInfo, on_delete = models.CASCADE)
+
+class Suggestion(models.model):
+suggestion = models.CharField(max_length = 100)
+
+class Notification(models.Model):
+company_name = models.CharField(max_length=500)
+date = models.TextField()
+notification = models.CharField(max_length = 100)
 
 
-        def __unicode__(self):
-               return u'%s, %s, %s' %(self.cnum, self.cname, self.credits)
-class section(models.Model):
-        snum = models.CharField(max_length=9, primary_key=True)
-        cnum = models.ForeignKey(course, db_column='cnum')
-        fid = models.ForeignKey(faculty, db_column='fid')
-        semester = model
-        s.CharField(max_length=6)
-        year = models.IntegerField()
-        enrollment = models.IntegerField()
-        capacity = models.IntegerField()
-        
-        def __unicode__(self):
-               return u'%s, %s' % (self.snum, self.enrollment) 
-
-
-//settings.py//related to database
-
-DATABASE_ENGINE = 'mysql'
-DATABASE_NAME = 'sis'
-DATABASE_USER = 'micsuser'
-DATABASE_PASSWORD = 'micspass' 
