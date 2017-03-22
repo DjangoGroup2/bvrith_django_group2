739c6188a98bc49d64360689c32fbd8886215428
BommireddyJahnavi <jahnavi160897@gmail.com>
2017-03-22 20:35:50 +0530

    Update models.py

--- models.py
+++ models.py
@@ -1,10 +1,10 @@
 from django.db import models
 
-class tpo(models.Model):
-	rollno = models.CharField(max_length = 10)
-	name = models.CharFeild(max_length = 30)
-	mail = models.CharFeild(max_length = 50)
-	phonenumber = models.IntegerField()
+class Notification(models.Model):
+    student = models.ForeignKey(student,max_length=50)
+    company_name = models.CharField(max_length=500)
 
-	class Meta:
-		db_table = "tpomodule"
+class Drive(models.Model):
+    company_name = models.CharField(max_length=500)
+    release_date = models.DateField()
+    
